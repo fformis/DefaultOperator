2b2e1d91f469759ddc1a6d4600a8b0871bf59e31
Flávio José Formis <flavio.formis@pca.com.br>
2020-10-26 16:09:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefaultOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int teste; //Erro de compilação
+            //int teste = 0; //Valor hardcode
+            int teste = default; // valor padrão da linguagem
+            
+            string teste2 = default; //null
+            Console.WriteLine(teste);
+            Console.WriteLine(teste2??"era null");
+        }
+    }
+}
